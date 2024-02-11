774dccd082b2207b5c9d7678dc797852d58e6e35
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-02-11 21:06:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-function fibonacci(num) {
+function fibonacci(n) {
 // your code here
-	let a=0,b=1,c;
-	for(let i=0;i<num;i++){
-		c=a+b;
-		a=b;
-		b=c;
-	}
-	return a;
+	if (n == 0 || n == 1) {
+        return n;
+    }
+    // Element 1
+    let element1 = 0;
+    // Element 2
+    let element2 = 1;
+    // Move the 2 elements window till one index before the nth.
+    for (let i = 2; i < n; i++) {
+        // Move the 2 elements window forward.
+        let temp = element2;
+        element2 = element1 + element2;
+        element1 = temp;
+    }
+    // Return the nth Fibonacci by summing Fibonacci n-1 and n-2
+    return element1 + element2;
 	
 	
 }
