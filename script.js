ba49689601f66e6fee497b66d7a2ab4c3b99a8ac
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-02-11 21:15:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function fibonacci(n) {
 // your code here
-	if (n == 0 || n == 1) {
-        return n;
-    }
+	if (n == 1 ) {
+        return 0;
+    }else if(n==2){
+		return 1;
+	}
     // Element 1
     let element1 = 0;
     // Element 2
