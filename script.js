d41e65ac685b2c04b9c4a7c41d0a19ff134cf48c
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-03-16 17:02:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function fibonacci(n) {
 // your code here
 	 let a = 0, b = 1;
     if (n === 1) return a;
-    for (let i = 2; i <= n; i++) {
+	if (n === 2) return b;
+    for (let i = 3; i <= n; i++) {
         let temp = b;
         b = a + b;
         a = temp;
